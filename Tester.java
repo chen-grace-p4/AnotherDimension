031b3843e76d1e081aae07ab058e5df05129b4e4
Grace <gchen20@stuy.edu>
2020-10-23 13:17:31 -0400

    I have created and tested 4b largest()

--- Tester.java
+++ Tester.java
@@ -1,6 +1,7 @@
 public class Tester{
   public static void main(String[]args) {
-    int[] TesterA = {1,2,3,4};
-    System.out.print(ArrayOps.sum(TesterA));
+    int[] TesterA = {100,9,8,0,3,4,990};
+    //System.out.print(ArrayOps.sum(TesterA));
+    System.out.print(ArrayOps.largest(TesterA));
   }
 }
